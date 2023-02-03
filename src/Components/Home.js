54c1bced1941f3f85dcd17b84f4a25c5dd539cd5
A.chitrarasu <chitrarasumit@gmail.com>
2023-02-03 12:39:52 +0530

    React Day 1 task

---
+++ src/Components/Home.js
@@ -0,0 +1,28 @@
+const Home =()=>{
+    return(
+     <div>
+      <div class="container text-center ">
+  <div class="row">
+    <div class="col border">
+    <h5 class="card-title text-muted">Pro</h5>
+<h5 className="card-tittle"> $49/month</h5>
+<ul className=" fa-ul">
+<li><span class="fa-li"><i class="fas fa-check"></i></span><strong>Unlimited Users</strong>
+              </li>
+              </ul>
+    </div>
+
+
+    <div class="col">
+      Column
+    </div>
+    <div class="col">
+      Column
+    </div>
+  </div>
+</div>
+   </div> 
+    )
+    
+};
+export default Home;
