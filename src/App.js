27dc096da2245584b10c8cf02d693931ae0797c7
A.chitrarasu <chitrarasumit@gmail.com>
2023-02-03 13:13:27 +0530

    react day1 task

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Pricecard from './Components/Pricecard';
-import Home from './Components/Home';
+
 function App() {
   return (
     <div className="App">
